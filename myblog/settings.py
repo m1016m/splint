74cb4cad58e3a7b6a8b09372ac34d9943fffe40a
m1016m <m1016m@gmail.com>
2024-02-17 13:43:34 +0800

    Add files via upload

--- myblog/settings.py
+++ myblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-09=^tiartu1+%re#3gt8hqfy-nr%nslrzr=dl%mzl%mua8_e&n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*","sp-jc3j.onrender.com"]
 
 
 # Application definition
